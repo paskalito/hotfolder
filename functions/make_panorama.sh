4a23ee66bbabd9229b98f92f4effe1e3f04d5262
paskalito <paskalito@noththere.com>
2016-01-23 00:04:20 +0100

    added panoramastichting function

---
+++ functions/make_panorama.sh
@@ -0,0 +1,23 @@
+
+NOW=$(date +"%Y-%m-%d %H:%M")
+
+pto_gen *.jpg -o project.pto
+
+cpfind --multirow -o project.pto project.pto
+
+celeste_standalone -i project.pto -o project.pto
+
+# not working
+# ptoclean -v --output project.pto project.pto
+
+autooptimiser -a -l -s -o project.pto project.pto
+
+pano_modify -o project.pto --center --straighten --canvas=AUTO --crop=AUTO project.pto
+
+nona -m TIFF_m -o project project.pto
+
+enblend -o project.jpg *.tif
+
+mv project.jpg ../out/panorama-"$NOW".jpg
+
+rm *
