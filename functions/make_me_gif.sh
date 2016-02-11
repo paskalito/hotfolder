c6305e0523007c0f95778c7bd0a2fd865c103579
paskalito <paskalito@noththere.com>
2016-02-11 22:18:00 +0100

    added function to create animated gifs

---
+++ functions/make_me_gif.sh
@@ -0,0 +1,27 @@
+# Info
+# In order to use this "function" need to set make_me_gif.sh in line 36 in the hotfolder.sh
+# You need to have ImageMagick installed on your system. (http://www.imagemagick.org/)
+
+
+# choose desired width of the image (in pixel) if you don't want to resize or only resize height leave blank (usefull for panorama's)
+IMAGEWIDTH=2200
+# choose desired height of the image (in pixel) if you don't want to resize or only resize width leave blank
+IMAGEHEIGHT=2200
+# choose the delay you want between the pictures. ( i really don't know what the value stands for..) (they call it ticks..) (http://www.imagemagick.org/script/command-line-options.php#delay)
+DELAY=5
+
+
+# ------------------------ code ---------------------------------------------
+# generate date and time variabel for the output folders
+NOW=$(date +"%Y-%m-%d %H:%M")
+
+# the ImageMagick convert code
+mogrify -verbose -format jpg -resize ''$IMAGEWIDTH'x'$IMAGEHEIGHT'>' *
+# the ImageMagick code who converts the pictures into the animated gif
+convert -delay "$DELAY" * animated.gif
+# make a new dir for the files to be safed in
+mkdir ../out/Conversion-"$NOW"
+# move all the files of your choosen output filetype into that folder
+mv *.gif ../out/Conversion-"$NOW"/
+# delete the rest (former files is imageformat was changed) // cleanup the process folder
+rm *
