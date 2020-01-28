b10279528119ad231e4632c1ee3e1cb86cd45652
paskalito <paskalito@users.noreply.github.com>
2020-01-28 23:17:09 +0100

    Changed how everything works + usability +compatibilty
    Added another mecanism to crawl folder state
    replaced source with . for bigger comatibilty
    now easily change what your hotfolder should do on the very top of the script

--- hotfolder.sh
+++ hotfolder.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# What should this instance of hotfolder do?
+# Fill in the name of the function you want to use
+FUNCTION=convert_video
+
+# The Following options are available:
+
+# convert_picures - Convert and Resize Pictures (uses ImageMagik)
+# convert_video - Convert/Compress Videos (uses HandBrake_CLI)
+# make_me_gif - Convert a bunge of picures into an animated gif (choose your outputsize) (uses ImageMagik)
+# compress_pdf - Compress PDF's (uses Ghostscript)
+# make_panorama - [EXPERIMENTAL] Stiches panorama Shots toghether into one Panorama Image (uses hugin)
+
+
+
+
+# ------------------------ code (Do not edit below) ---------------------------------------------
+
 # Finding path to itself
 RELPATH=$(dirname "$0")
 
@@ -13,40 +30,53 @@ mkdir in out processing
 # mechanism to restart converting videos if script was aborted(computer shutdown)
 FOLDERSIZE=`du -s processing | cut -f1`
 
-find processing/ -type f -iname *_converted* -exec rm -f {} \;
+# find processing/ -type f -iname *_converted* -exec rm -f {} \;
+find processing/ -type f \( -name '*_converted*' -or -name '*_compressed*' \) -exec rm -f {} \;
 mv processing/* in
+
 
 
 #open a loop
 while :
 do
 
-  #check the size of folder in, wait 5 second check again.
-  FOLDERSIZE1=`du -s in | cut -f1`
-  echo $FOLDERSIZE1
-  sleep 2
-  FOLDERSIZE2=`du -s in | cut -f1`
-  echo $FOLDERSIZE2
-  sleep 2
 
-  # if the first and secod time the foldersize didn't change and is grater than 0 start processing
-	if [ "$FOLDERSIZE1" -eq "$FOLDERSIZE2" ] && [ "$FOLDERSIZE2" -gt 0 ]; then
-  # Start Processing > Enter your code here
+if ! [ "$(ls -A in)" ]; then
+	echo 'The "in" Folder is Empty'
+	# to reduce processing load lets wait some seconds till we check again.	
+	sleep 5
+else
+	echo 'Oh something is in the "in" Folder let us start then'
+	#check the size of folder in, wait 5 second check again.
+	FOLDERSIZE1=`du -s in | cut -f1`
+	echo $FOLDERSIZE1
+	sleep 10
+	FOLDERSIZE2=`du -s in | cut -f1`
+	echo $FOLDERSIZE2
+	sleep 2
+
+		# if the first and secod time the foldersize didn't change and is grater than 0 start processing (we then assume that the copying into the in folder is finished)
+		if [ "$FOLDERSIZE1" -eq "$FOLDERSIZE2" ] && [ "$FOLDERSIZE2" -gt 0 ]; then
+			# Start Processing > Enter your code here
+		  
+			# move content from in folder to processing folder
+			mv in/* processing
+			# go into processing folder / make it current path
+			cd ./processing
+			  
+			# load a script from functions / here it actually starts the processing of the data
+			# replaced source with . for bigger compatibility
+			. "$RELPATH"/functions/"$FUNCTION".sh
+
+			# go up one level (because in the rest of the script we assume to be at the base level of our folderstructure)
+			cd ..
+
+		else
+			# this is somehow obsolete now but if there was a file with size 0 placed in the in folder this is where we would land.
+			echo Size of in is still changing we assume someone is still copying their data into it.
+			echo OR someone placed a file with size 0 in the in folder
+		fi
+fi
+
   
-  # move content from in folder to processing folder
-  mv in/* processing
-  # go into processing folder / make it current path
-  cd ./processing
-  
-  # load a script from functions / here it actually starts the processing of the data
-  source "$RELPATH"/functions/convert_video.sh
-
-  # go up one level
-  cd ..
-
-  else
-  # if folder is empty or something is being copied into it. (within the last 5 sec it changed it's size)
-  # This is what get's executed while "waiting".
-  echo nothing here
-  fi
 done
