e8dcff605a9f02976254603f2a74efd5e90a2a8d
paskalito <paskalito@users.noreply.github.com>
2020-01-28 23:19:04 +0100

    updated to new mecanism

--- functions/convert_pictures.sh
+++ functions/convert_pictures.sh
@@ -1,9 +1,9 @@
 # Info
-# In order to use this "function" need to set convert_pictures.sh in line 36 in the hotfolder.sh
+# In order to use this "function" need to set convert_pictures for the FUNCTION variabel in line 5 of the hotfolder.sh file
 # You need to have ImageMagick installed on your system. (http://www.imagemagick.org/)
 
 # choose one of the 200+ formats ImageMagick supports (http://www.imagemagick.org/script/formats.php)
-OUTPUTFORMAT=png
+OUTPUTFORMAT=jpg
 # choose desired width of the image (in pixel) if you don't want to resize or only resize height leave blank (usefull for panorama's)
 IMAGEWIDTH=2200
 # choose desired height of the image (in pixel) if you don't want to resize or only resize width leave blank
