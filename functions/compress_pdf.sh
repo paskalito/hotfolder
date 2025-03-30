797faf465470cb740609049922f037d186055fa4
paskalito <paskalito@users.noreply.github.com>
2025-03-30 13:20:56 +0200

    Update compress_pdf.sh
    v0.2 -2025-03-20 - You can set custom DPI Settings to override the ones in the Presets.

--- functions/compress_pdf.sh
+++ functions/compress_pdf.sh
@@ -1,6 +1,8 @@
 # INFO
 # In order to use this "function" need to set compress_pdf in line 5 in the hotfolder.sh
 # You need to have Ghostscript (in fact the command gs) installed and in stadardpath
+# Changlog
+# v0.2 -2025-03-20 - You can set custom DPI Settings to override the ones in the Presets.
 
 # theese are the quality settings you can choose
 
@@ -13,7 +15,7 @@
 
 # choose name of the preset you wanna use
 GHOSTSCRIPTPRESET="ebook"
-
+DPI= # Leave blank if you dont want to override DPI settings from the preset
 
 # ------------------------ code ---------------------------------------------
 
@@ -21,13 +23,18 @@ if hash gs 2>/dev/null; then
 	for filename in *.*; do
 		# the ghostcript compress command
 		# gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -sOutputFile=output2.pdf $filename
-		gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/${GHOSTSCRIPTPRESET%.*} -sOutputFile="${filename%.*}_compressed_${GHOSTSCRIPTPRESET%.*}.pdf" "$filename"
-		mv "${filename%.*}_compressed_${GHOSTSCRIPTPRESET%.*}.pdf" ../out/
+		if [ $DPI -ne $null ]; then
+			OUTPUTNAME="${filename%.*}_compressed_${GHOSTSCRIPTPRESET}.pdf"
+			gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/${GHOSTSCRIPTPRESET} -sOutputFile="$OUTPUTNAME" "$filename"
+		else
+			OUTPUTNAME="${filename%.*}_compressed_${GHOSTSCRIPTPRESET}_${DPI}_DPI.pdf"
+			gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/${GHOSTSCRIPTPRESET} -dEmbedAllFonts=true -dSubsetFonts=true -dColorImageDownsampleType=/Bicubic -dColorImageResolution=$DPI -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=$DPI -dMonoImageDownsampleType=/Bicubic -dMonoImageResolution=$DPI -sOutputFile="$OUTPUTNAME" "$filename"
+		fi		
+		mv "$OUTPUTNAME" ../out/
+		echo "Compression finished."
 		rm "$filename"
 	done
 else
 	echo ERROR: Could not find GhostScript >&2
 	echo GhostScript needs to installed and in standardpath for compress_pdf to work >&2
 fi
-
-	
